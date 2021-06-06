91825f365e1ab5b0151ea9d62a0bb756ba94d811
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-06 17:58:59 +0530

    Added: Calculating length of a line

--- LineComparison.java
+++ LineComparison.java
@@ -1,21 +1,23 @@
 /**
- *  Compilation:  javac -d bin Line_Comparison.java
- *  Purpose: Use Java Object Oriented Programming Concepts of Line and Point as well as equals and compareTo methods.
- *  @author  Dhiraj
- *  @version 1.0
- *  @since   06-06-2021
- * */
+ * Compilation:  javac -d bin Line_Comparison.java
+ * Purpose: Use Java Object Oriented Programming Concepts of Line and Point as well as equals and compareTo methods.
+ *
+ * @author Dhiraj
+ * @version 1.0
+ * @since 06-06-2021
+ */
 
 package bridgelabz;
+
 class Line {
-    int x1=3, x2=6, y1=8, y2=2;
+    int x1 = 3, x2 = 6, y1 = 8, y2 = 2;
     double lineLength;
 
     /**
      * Method for calculating the length of line.
      */
     public void calculatingLengthOfLine() {
-        lineLength = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        lineLength = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     }
 }
 
