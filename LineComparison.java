115cd8b1ca26669cc2b1b70adb2dda0532fef7f2
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-07 14:02:54 +0530

    Added:Checking equality of two lines

--- LineComparison.java
+++ LineComparison.java
@@ -10,18 +10,35 @@
 package bridgelabz;
 
 public class LineComparison {
-    public static double lineLength;
+    public static double firstLineLength;
+    public static double secondLineLength;
 
     /**
      * Method for calculating the length of line.
      */
-    private static void calculatingLengthOfLine(int x1, int x2, int y1, int y2) {
-        lineLength = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    private static double calculatingLengthOfLine(int x1, int x2, int y1, int y2) {
+        double lineLength;
+        lineLength = Math.sqrt(Math.pow(x2 - x1, 2) + (Math.pow(y2 - y1, 2)));
+        return lineLength;
+    }
+
+    /**
+     * Method for checking equality of two lines.
+     */
+    private static void checkingEqualityOfLine() {
+        if (firstLineLength == secondLineLength) {
+            System.out.println("Both Lines are equal");
+        } else {
+            System.out.println("Lines are not equal");
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome To Line Comparison Computation Program on Master Branch");
-        calculatingLengthOfLine(3, 2, 6, 8);
-        System.out.println(lineLength);
+        firstLineLength = calculatingLengthOfLine(3, 2, 6, 8);
+        System.out.println(firstLineLength);
+        secondLineLength = calculatingLengthOfLine(5,3,6,2);
+        System.out.println(secondLineLength);
+        checkingEqualityOfLine();
     }
 }
