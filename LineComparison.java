a8825402125672807b844b5cebe8c966351e10e7
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-07 13:29:23 +0530

    Added:calculating the length of line

--- LineComparison.java
+++ LineComparison.java
@@ -9,24 +9,19 @@
 
 package bridgelabz;
 
-class Line {
-    int x1 = 3, x2 = 6, y1 = 8, y2 = 2;
-    double lineLength;
+public class LineComparison {
+    public static double lineLength;
 
     /**
      * Method for calculating the length of line.
      */
-    public void calculatingLengthOfLine() {
+    private static void calculatingLengthOfLine(int x1, int x2,int y1 , int y2 ) {
         lineLength = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     }
-}
-
-public class LineComparison {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Line Comparison Computation Program on Master Branch");
-        Line lineObject = new Line();
-        lineObject.calculatingLengthOfLine();
-        System.out.println(lineObject.lineLength);
+        calculatingLengthOfLine(3,2,6,8);
+        System.out.println(lineLength);
     }
 }
--- README.md
+++ README.md
@@ -2,9 +2,5 @@
 
 **UC1-CalculatingLentghofLine**
 
-_**Description**_ : Created a object named "Line" , Inside of Line Class or Object Created a method named "calculatingLengthOfLine" for Calculating the length of line using points.
-
-**UC2-CheckingEquality**
-
-_**Description**_ : Created a method named "CheckingEqualityOfLines" for checking equality of two lines.
+_**Description**_ : Model a line based on a point consisting of (x, y) co-ordinates using the Cartesian system, So that I can calculate its length.
 
